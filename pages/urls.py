6e5cd69f22304857846ae5f112d1905c81c16603
Sharad Bhandari <chemist.sharad@gmail.com>
2020-06-16 20:21:25 +0545

    upto template completion of project

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home,about
 
 
 app_name = 'pages'
 
 urlpatterns = [
 	path('',home,name='home'),
+	path('/about',about,name='about'),
 
 ]
