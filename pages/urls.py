4625b34db94dce8a457fafe3f6e1160abd831085
Sharad Bhandari <chemist.sharad@gmail.com>
2020-07-12 09:40:59 +0545

    frontend

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import home,about
+from .views import home,about,services,contacts,country,economic,politics,science,startup
 
 
 app_name = 'pages'
 
 urlpatterns = [
 	path('',home,name='home'),
-	path('/about',about,name='about'),
+	path('about/',about,name='about'),
+	path('servies/',services,name='services'),
+	path('contacts/',contacts,name='contacts'),
+	path('country/',country,name='country'),
+	path('economic/',economic,name='economic'),
+	path('politics/',politics,name='politics'),
+	path('science/',science,name='science'),
+	path('startup/',startup,name='startup'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -10,3 +10,31 @@ def home(request):
 def about(request):
 
 	return render(request,'about.html')
+
+def services(request):
+
+	return render(request,'services.html')
+
+def contacts(request):
+
+	return render(request,'contacts.html')
+
+def country(request):
+
+	return render(request,'country.html')
+
+def economic(request):
+
+	return render(request,'economic.html')
+
+def politics(request):
+
+	return render(request,'politics.html')
+
+def startup(request):
+
+	return render(request,'startup.html')
+
+def science(request):
+
+	return render(request,'science.html')
